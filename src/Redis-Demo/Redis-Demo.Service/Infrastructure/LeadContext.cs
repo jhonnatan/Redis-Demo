fc90914eeb6eb6b946777b919fc1c8a590706670
Jhonnatan <jhonnatanprado@outlook.com>
2020-11-18 14:43:17 -0300

    Get data from Redis

--- src/Redis-Demo/Redis-Demo.Service/Infrastructure/LeadContext.cs
+++ src/Redis-Demo/Redis-Demo.Service/Infrastructure/LeadContext.cs
@@ -3,6 +3,7 @@ using Redis_Demo.Service.Domain;
 using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.Json;
 
 namespace Redis_Demo.Service.Infrastructure
@@ -25,6 +26,22 @@ namespace Redis_Demo.Service.Infrastructure
             {
                 dbRedis.StringSet($"id-{lead.Id}", JsonSerializer.Serialize(lead), expiry: null);
             }
+        }
+
+        public string GetLeadByKey(string key)
+        {
+            var connection = ConnectionMultiplexer.Connect(redisConnectionStr);
+            var dbRedis = connection.GetDatabase();
+
+            return dbRedis.StringGet(key);
+        }
+
+        public List<string> GetLeadsByKeys(List<string> keys)
+        {
+            var connection = ConnectionMultiplexer.Connect(redisConnectionStr);
+            var dbRedis = connection.GetDatabase();
+            var redisKeys = keys.Select(s=> new RedisKey(s)).ToArray();
+            return dbRedis.StringGet(redisKeys).Select(s=>s.ToString()).ToList();
         }
     }
 }
--- src/Redis-Demo/Redis-Demo.Service/Program.cs
+++ src/Redis-Demo/Redis-Demo.Service/Program.cs
@@ -4,6 +4,7 @@ using Redis_Demo.Service.Infrastructure;
 using Redis_Demo.Service.Modules;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 
 namespace Redis_Demo.Service
@@ -15,7 +16,7 @@ namespace Redis_Demo.Service
         {
             Console.WriteLine("Service Started");
             var container = RegisterContainers();
-
+            
             Console.WriteLine("Preparing Leads to Insert");
             var leads = new List<Lead>()
             {
@@ -23,11 +24,20 @@ namespace Redis_Demo.Service
                 new Lead(Guid.NewGuid(), "222222222", "Customer1", 43, "11 4321-7766"),
                 new Lead(Guid.NewGuid(), "333333333", "Customer1", 55, "11 98789-1243"),
             };
+
+            // Inserting in Redis
             var leadContext = new LeadContext();
             leadContext.InsertLeads(leads);
-
             Console.WriteLine("Leads Inserted in Redis");
 
+            // Get value by key in Redis
+            var firstLeadInserted = leadContext.GetLeadByKey($"id-{leads.First().Id}");
+            Console.WriteLine($"First lead Inserted: {firstLeadInserted}");
+
+            // Get values by keys in Redis
+            var allLeads = leadContext.GetLeadsByKeys(leads.Select(s => $"id-{s.Id}").ToList());
+            Console.WriteLine("All Leads inserted:");
+            allLeads.ForEach(s => Console.WriteLine(s));
 
             AppDomain.CurrentDomain.ProcessExit += (o, e) =>
             {                
